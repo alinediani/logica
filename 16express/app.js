e82485644215b8390709bb7537674a6c86c70c75
Aline Diani <63258951+alinediani@users.noreply.github.com>
2020-08-13 20:40:41 -0300

    atualização express

--- 16express/app.js
+++ 16express/app.js
@@ -1,11 +1,31 @@
 const express=require('express')
+const bp= require('body-parser')
 const app= express();
 const router=express.Router();
+app.use(bp.json());
+app.use(bp.urlencoded({ extended:false}));
+
 const route =router.get('/',(req,res,next)=>{
     res.status(200).send({
        title: "Node Store API",
        version:"0.0.1"
     });
 });
+const create= router.post('/',(req,res,next)=>{
+    res.status(200).send();
+});
+const put = router.put('/:id',(req,res,next)=>{
+    const id=req.params.id;
+    res.status(200).send({
+        id:id,
+        item: req.body
+    })
+})
+const del= router.delete('/',(req,res,next)=>{
+    res.status(200).send();
+});
 app.use('/',route)
+app.use('/products',create)
+app.use('/products',put)
+app.use('/products',del)
 module.exports=app;
